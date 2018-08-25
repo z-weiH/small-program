362a7b38e8bf07093dbdf0a363075929113698c3
zengwei <18279047775@163.com>
2018-08-25 17:03:46 +0800

    啦啦啦

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World243',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    demoValue: 3,
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,21 +18,14 @@ Page({
     })
   },
   onLoad: function () {
+    wx.hideTabBar();
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
+    }else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
@@ -50,5 +45,13 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+
+  onShareAppMessage() {
+    console.log('分享');
+    return {
+      title: '自定义转发标题',
+      path: '/page/logs?id=123'
+    }
+  },
 })
